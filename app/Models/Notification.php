3d539e1c455ad9ab463df63c55e9496d002d62a8
konst.cos <konst.cos@gmail.com>
2016-07-22 11:10:44 +0300

    create method Notification::make( $sender, $event )

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
-}
+    /**
+     * Add a new record to the table 'notifications'
+     *
+     * @param  int  $sender
+     * @param  string  $event
+     * @return object
+     */
+    public static function make( $sender, $event )
+    {
+        $notice = new Notification;
+        $notice->sender_id = $sender;
+        $notice->event = $event;
+        $notice->save();
+        return $notice;
+    }}
