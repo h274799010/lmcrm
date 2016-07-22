012bfd39aa953100263cf8da06f4c9656c529c51
konst.cos <konst.cos@gmail.com>
2016-07-22 10:41:52 +0300

    make migration notifications and notificate_users

---
+++ database/migrations/2016_07_22_072742_create_notifications.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotifications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sender_id');
+            $table->string('event',15);
+            $table->text('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notifications');
+    }
+}
