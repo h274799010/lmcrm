ef61ffbeaf70aaecbddc5b3a783caeca93f57a76
konst.cos <konst.cos@gmail.com>
2016-07-22 12:46:52 +0300

    create a method Notification_users::takenByNotice and a bit of small improvements

--- app/Models/Notification_users.php
+++ app/Models/Notification_users.php
@@ -13,13 +13,32 @@ class Notification_users extends Model
      * @param  object $notice
      * @return object
      */
-    public static function make($user, $notice)
+    public static function make( $user, $notice )
     {
 
         $userNotice = new Notification_users;
         $userNotice->notification_id = $notice->id;
         $userNotice->user_id = $user->id;
+        $userNotice->notified = 0;
         $userNotice->save();
         return $userNotice;
     }
+
+    /**
+     * Set users notified=1 by $notice
+     *
+     * @param  object $user
+     * @param  object $notice
+     * @return object
+     */
+    public static function takenByNotice( $user, $notice )
+    {
+        $userNotice = self::where('user_id', '=', $user)->where('notification_id', '=', $notice)->first();
+        $userNotice->notified = 1;
+        $userNotice->save();
+
+        return $userNotice;
+    }
+
+
 }
