d7bcce36f01f3947761d6c5c7dab6c9fb58ca55b
konst.cos <konst.cos@gmail.com>
2016-07-22 11:34:19 +0300

    create method Notification_users::make( $user, $notice )

--- app/Models/Notification_users.php
+++ app/Models/Notification_users.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification_users extends Model
 {
-    //
+    /**
+     * Add a new record to the table 'notification_users'
+     *
+     * @param  object $user
+     * @param  object $notice
+     * @return object
+     */
+    public static function make($user, $notice)
+    {
+
+        $userNotice = new Notification_users;
+        $userNotice->notification_id = $notice->id;
+        $userNotice->user_id = $user->id;
+        $userNotice->save();
+        return $userNotice;
+    }
 }
