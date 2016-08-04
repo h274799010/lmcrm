11668f843b6208b9321f66fcdd9a6ff1024fc966
Oleg <kaktus_mov@mail.ru>
2016-08-04 17:17:41 +0300

    test2

--- app/Http/Controllers/Agent/LeadController.php
+++ app/Http/Controllers/Agent/LeadController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Agent;
 
 use App\Http\Controllers\AgentController;
+use App\Models\Organizer;
 use App\Models\SphereMask;
+use App\Models\SphereStatuses;
 use Validator;
 use App\Models\Agent;
 use App\Models\Salesman;
@@ -11,6 +13,7 @@ use App\Models\Credits;
 use App\Models\Lead;
 use App\Models\Customer;
 use App\Models\Sphere;
+use App\Models\OpenLeads;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 //use App\Http\Requests\Admin\ArticleRequest;
@@ -84,6 +87,9 @@ class LeadController extends AgentController {
             ->edit_column('id',function($model){
                 return view('agent.lead.datatables.obtain_open',['lead'=>$model]);
             })
+            ->add_column('ids',function($model){
+                return view('agent.lead.datatables.obtain_open_all',['lead'=>$model]);
+            },2)
             ->edit_column('status',function($model){
                 return '';
             })
@@ -96,7 +102,7 @@ class LeadController extends AgentController {
         $lead_attr = $agent->sphere()->leadAttr()->get();
         foreach($lead_attr as $key=>$l_attr){
            $datatable->add_column('a_'.$key,function($lead) use ($l_attr){
-                $val = $lead->info()->where('key','=',$l_attr->id)->first();
+                $val = $lead->info()->where('lead_attr_id','=',$l_attr->id)->first();
                 return view('agent.lead.datatables.obtain_data',['data'=>$val,'type'=>$l_attr->_type]);
            });
         }
@@ -113,20 +119,87 @@ class LeadController extends AgentController {
         $price = $mask->findMask()->sharedLock()->first()->lead_price;
 
         if($price > $balance) {
-            return redirect()->route('agent.lead.obtain',[0]);
+            return 'Error: low balance';
+            //return redirect()->route('agent.lead.obtain',[0]);
         }
 
         $lead = Lead::lockForUpdate()->find($id);
         if($lead->sphere->openLead > $lead->opened) {
-            $lead->obtainedBy()->attach($this->uid);
-            $lead->opened+=1;
+            //$lead->opened+=1;
+            //$credit->history()->save(new CreditHistory());
+
+            $updateCount = Lead::where('id',$lead->id)->where('opened','<',$lead->sphere->openLead)->increment('opened');
+            if($updateCount){
+                //$lead->obtainedBy()->attach($this->uid);
+                $ol = OpenLeads::where(['lead_id'=>$id,'agent_id'=>$this->uid])->first();
+                if (!$ol){
+                    $ol = new OpenLeads();
+                    $ol->lead_id = $id;
+                    $ol->agent_id = $this->uid;
+                    $ol->save();
+                }
+                else
+                {
+                    $ol->increment('count');
+                }
+                $credit->payment=$price;
+                $credit->save();
+                return 'Successfully obtained';
+            }
+            else{
+                return 'Obtain error';
+            }
+        }
+        else
+        {
+            return 'Превышен лимит: открыто '.$lead->opened.' из '.$lead->sphere->openLead;
+        }
+    }
+
+    public function openAllLeads($id){
+        $agent = $this->user;
+        $agent->load('bill');
+        $credit = Credits::where('agent_id','=',$this->uid)->sharedLock()->first();
+        $balance = $credit->balance;
+
+        $mask=$this->mask;
+
+        $lead = Lead::lockForUpdate()->find($id);
+        $ol = OpenLeads::where(['lead_id'=>$id,'agent_id'=>$this->uid])->first();
+        $obtainedByThisAgent = 0;
+        if ($ol)
+            $obtainedByThisAgent = $ol->count;
+        if ($lead->opened > 0 && $lead->opened != $obtainedByThisAgent)
+            return 'Part of leads is already obtained by other agent';
+
+        $mustBeAdded = $lead->sphere->openLead - $obtainedByThisAgent;
+        $price = $mask->findMask()->sharedLock()->first()->lead_price*$mustBeAdded;
+
+        if($price > $balance) {
+            return 'Error: low balance';
+            //return redirect()->route('agent.lead.obtain',[0]);
+        }
+
+        //$lead->opened += $lead->sphere->openLead;
+        $updateCount = Lead::where('id',$lead->id)->where('opened',$lead->opened)->increment('opened',$mustBeAdded);
+        if ($updateCount)
+        {
+            //$lead->obtainedBy()->attach($this->uid);
+            if (!$ol){
+                $ol = new OpenLeads();
+                $ol->lead_id = $id;
+                $ol->agent_id = $this->uid;
+            }
+            $ol->count = $lead->sphere->openLead;
+            $ol->save();
             $credit->payment=$price;
             $credit->save();
-            //$credit->history()->save(new CreditHistory());
+            return 'Successfully obtained';
         }
-        $lead->save();
-
-        return redirect()->route('agent.lead.obtain');
+        else{
+            return 'Obtain error';
+        }
+        //$credit->history()->save(new CreditHistory());
     }
 
     /**
@@ -189,6 +262,86 @@ class LeadController extends AgentController {
         return response()->route('agent.lead.index');
     }
 
+    public function openedLeads(){
+        $dataArray = Lead::has('obtainedBy')->get();
+        return view('agent.lead.opened',['dataArray'=>$dataArray]);
+    }
 
+    public function openedLeadsAjax(){
+        $id = $_GET['id'];
+        $data = Lead::has('obtainedBy')->find($id);
+        $arr[] = ['date',$data->date];
+        $arr[] = ['name',$data->name];
+        $arr[] = ['phone',$data->phone->phone];
+        $arr[] = ['email',$data->email];
+        foreach ($data->sphereAttr as $key=>$sphereAttr){
+            $str = '';
+            foreach ($sphereAttr->options as $option){
+                $mask = new SphereMask($data->sphere_id,$data->id);
+                $resp = $mask->where('type','lead')->where('fb_'.$option->sphere_attr_id.'_'.$option->id,1)->get()->toArray();
+                if (count($resp))
+                    $str .= $option->value;
+            }
+            $arr[] = [$sphereAttr->label,$str];
+        }
+        echo json_encode(['data'=>$arr]);exit;
+    }
 
+    public function showOpenedLead($id){
+        $openedLead = OpenLeads::where(['lead_id'=>$id,'agent_id'=>$this->uid])->first();
+        return view('agent.lead.openedLead')->with('openedLead',$openedLead);
+    }
+
+    public function editOpenedLead(Request $request){
+        $openLead = OpenLeads::where(['id'=>$request->input('id'),'agent_id'=>$this->uid])->first();
+        $openLead->comment = $request->input('comment');
+        $openLead->save();
+        return redirect()->back();
+    }
+
+    public function nextStatus($id){
+        $openedLead = OpenLeads::where(['lead_id'=>$id,'agent_id'=>$this->uid])->first();
+        $status = $openedLead->status+1;
+        if ($openedLead->lead->sphere->statuses->where('position',$status)->first())
+            $openedLead->increment('status');
+        return redirect()->route('agent.lead.showOpenedLead',[$id]);
+    }
+
+    public function putReminder(Request $request){
+        $openLead = OpenLeads::where(['id'=>$request->input('open_lead_id'),'agent_id'=>$this->uid])->first();
+        if ($openLead)
+        {
+            $organizer = false;
+            if ($request->input('id')){
+                $organizer = Organizer::where(['id'=>$request->input('id')])->first();
+            }
+            if (!$organizer)
+            {
+                $organizer = new Organizer();
+                $organizer->open_lead_id = $request->input('open_lead_id');
+            }
+            $organizer->time = strtotime($request->input('time'));
+            $organizer->comment = $request->input('comment');
+            $organizer->save();
+        }
+        if($request->ajax()){
+            return 'reload';
+        } else {
+            return redirect()->route('agent.lead.showOpenedLead',$request->input('open_lead_id'));
+        }
+
+    }
+
+    public function deleteReminder($id){
+        $organizer = Organizer::where(['id'=>$id])->first();
+        if ($organizer->openLead->agent_id == $this->uid){
+            $organizer->delete();
+        }
+        return redirect()->route('agent.lead.showOpenedLead',$organizer->openLead->lead_id);
+    }
+
+    public function addReminder($open_lead_id)
+    {
+        return view('agent.lead.createReminder')->with('open_lead_id',$open_lead_id);
+    }
 }
