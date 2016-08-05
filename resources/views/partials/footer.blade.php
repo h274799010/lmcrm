b2557759cab1c3d793c5c2198843117c98a2e76b
konst.cos <konst.cos@gmail.com>
2016-08-05 17:50:10 +0300

    active menu, adaptability, and other minor changes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,6 +2,13 @@
     <div class="container">
         <p class="text-muted credit"><span style="text-align: left; float: left">&copy; 2016 <a href="#">LM CRM</a></span>
             <!--<span class="hidden-phone" style="text-align: right; float: right">Powered by: <a href="http://laravel.com/" >Laravel 5</a></span>-->
+
+            <br>
+
+            <a href="{{ route('agent.sphere.index') }}"> Filtration customer</a> |
+            <a href="{{ route('agent.salesman.index') }}"> Salesmen</a> |
+            <a href="{{ route('operator.sphere.index') }}" > Leads filter</a>
+
         </p>
     </div>
 </div>
