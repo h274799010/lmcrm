6fe0826fc994a6b38908e206b8a0e64077572508
Oleg <kaktus_mov@mail.ru>
2016-08-08 17:04:53 +0300

    q

---
+++ app/Models/CreditTypes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CreditTypes extends Model {
+    protected $table="credit_types";
+    const LEAD_PURCHASE = -1;
+    const LEAD_SALE = 2;
+    const EXTERNAL_REFILL = 3;
+}
