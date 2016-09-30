e3881a1d2743f23a40f751cb2f11d1a8c1c3647d
konst.cos <konst.cos@gmail.com>
2016-09-30 23:53:51 +0300

    test route for request

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,6 +25,14 @@ Route::get('notice', ['as' => 'notification', 'middleware' => ['auth', 'agent|sa
 Route::post('notified', ['as' => 'notified', 'middleware' => ['auth', 'agent|salesman'], 'uses' => 'NoticeController@notified']);
 
 
+
+
+
+
+/** todo Тестовое удалить */
+
+
+
 // todo тестовый, удалить
 //Route::post('ffffff', function(){
 //    //header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
@@ -54,27 +62,62 @@ Route::get('token', function(){
 
 });
 
+
+Route::get('postData', function(){
+
+    $token = csrf_token();
+
+    echo
+        '
+<html>
+
+    <head>
+
+        <title>Тест поста</title>
+        <script src="/components/jquery/jquery-2.min.js"></script>
+
+    </head>
+
+    <body>
+        получение поста с сервера (только через csrf-токен) <br>
+
+        <script>
+
+        $(function(){
+
+            // изменяем статусы на сервере
+            $.post( "http://lmcrm.cos/ffffff", { "_token": \'' .$token .'\'}, function( data ){
+
+                alert(data.resp);
+
+            });
+        });
+    </script>
+
+    </body>
+</html>
+
+
+
+
+    '
+    ;
+
+});
+
 //Route::post('ffffff', function(){
-Route::match(['options', 'post'], 'ffffff', function(){
+Route::match(['post','options'], 'ffffff', function(){
 //    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 //    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 //    header('Access-Control-Allow-Credentials: true');
-    header("Access-Control-Allow-Origin: *");
-
-//    echo '
-//
-//    tcho
-//
-//
-//    ';
-
+//    header("Access-Control-Allow-Origin: *");
 
 
 //    echo 'sdfsdf';
 ////    echo json_encode('12313');
 //    exit;
 
-    return response()->json(['k'=>'aaa']);
+    return response()->json(['resp'=>'ответ сервера']);
 //        ->header('Access-Control-Allow-Origin', '*')
 //        ->header('Allow-Control-Allow-Credentials', 'true');
 
