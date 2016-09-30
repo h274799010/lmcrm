77af3ee81879344cf09e9d7a048be3281325747c
konst.cos <konst.cos@gmail.com>
2016-09-30 19:54:06 +0300

    make header("Access-Control-Allow-Origin: *") on post route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -59,7 +59,7 @@ Route::match(['options', 'post'], 'ffffff', function(){
 //    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 //    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 //    header('Access-Control-Allow-Credentials: true');
-//    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: *");
 
 //    echo '
 //
