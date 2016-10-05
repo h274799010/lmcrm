2515f30eccdc607af8243f343bfc2e57d37f6135
konst.cos <konst.cos@gmail.com>
2016-10-05 13:51:53 +0300

    chenge in routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,16 +31,20 @@ Route::post('notified', ['as' => 'notified', 'middleware' => ['auth', 'agent|sal
 
 
 
+
+
+
+
 /** todo Тестовое удалить */
 
+/** Получение токена */
 Route::get('token', function(){
-
-    header("Access-Control-Allow-Origin: *");
 
     return csrf_token();
 });
 
 
+/** Залогинивание пользователя */
 Route::match(['post','options'], 'mobileLogin', function( Request $request ){
 
     $credentials = [
@@ -57,7 +61,7 @@ Route::match(['post','options'], 'mobileLogin', function( Request $request ){
 });
 
 
-
+/** Разлогинивание пользователя */
 Route::match(['post','options'], 'mobileLogout', function( Request $request ){
 
     Sentinel::logout();
@@ -66,158 +70,16 @@ Route::match(['post','options'], 'mobileLogout', function( Request $request ){
 });
 
 
-
-
-Route::get('postData', function(){
-
-    $token = csrf_token();
-
-    echo
-        '
-<html>
-
-    <head>
-
-        <title>Тест поста</title>
-        <script src="/components/jquery/jquery-2.min.js"></script>
-
-    </head>
-
-    <body>
-        получение поста с сервера (только через csrf-токен) <br>
-
-        <script>
-
-        $(function(){
-
-            // изменяем статусы на сервере
-            $.post( "/ffffff", { "_token": \'' .$token .'\'}, function( data ){
-
-                alert(data.resp);
-
-            });
-        });
-    </script>
-
-    </body>
-</html>
-
-
-
-
-    '
-    ;
-
-});
-
-
-Route::get('postDataRemote', function(){
-
-    $token = csrf_token();
-
-    echo
-        '
-<html>
-
-    <head>
-
-        <title>Тест поста</title>
-        <script src="/components/jquery/jquery-2.min.js"></script>
-
-    </head>
-
-    <body>
-        получение поста с сервера (только через csrf-токен) <br>
-
-        <script>
-
-        $(function(){
-
-            // изменяем статусы на сервере
-//            $.post( "/ffffff", { "_token": \'' .$token .'\'}, function( data ){
-
-            $.get("http://lmcrm.biz.tm/token", function( data ){
-
-                var token = data;
-
-
-                $.ajax({
-                    url: "http://lmcrm.biz.tm/ffffff",
-                    type: "post",
-                    data: {
-                        _token: token
-                    },
-                  beforeSend: function(xhr){
-                            xhr.setRequestHeader("XSRF-TOKEN","eyJpdiI6IlpPSXJtcm1HeDA5RUp2bkxYNWJpQkE9PSIsInZhbHVlIjoidUhzcTZPbEs5QzBYRlpkU2haamJ0SmlXSU1cL1VaWCtUcjhPcWFpdTFTM3VSS3g5eG5qZmhjM0dRb1ZWSGpcL0lZV0ZzRFdDQTVUS00xNzdaRG5BdUxPdz09IiwibWFjIjoiMmEzZTZkOTM3ZDM0ODdkY2VkNDE4YjQ0MDAwZjAwYzgzODFmZGMxOTRmMDcyYzc1NzY3Yjc3Mjk4MDBlNjg0NSJ9");
-                            xhr.setRequestHeader("Accept","application/json");
-                  },
-                  dataType: "json",
-                    success: function (data) {
-                        alert(data.resp);
-                    }
-                });
-
-
-//                $.post( "http://lmcrm.biz.tm/ffffff", { _token: token }, function( data ){
-//    //            $.post( "http://lmcrm.biz.tm/ffffff", {}, function( data ){
-//
-//
-//                    alert(data.resp);
-//
-//                });
-
-
-
-            });
-
-
-
-//            $.post( "http://lmcrm.biz.tm/ffffff", { "_token": \'' .$token .'\'}, function( data ){
-////            $.post( "http://lmcrm.biz.tm/ffffff", {}, function( data ){
-//
-//
-//                alert(data.resp);
-//
-//            });
-        });
-    </script>
-
-    </body>
-</html>
-
-
-
-
-    '
-    ;
-
-});
-
-
-
-Route::match(['post','options'], 'ffffff', function(){
-
-    return response()->json(['resp'=>'дата7']);
-
-} );
-
-
-
+/** Проверка авторизации пользователя */
 Route::get('loginTest', function(){
-
-//        header("Access-Control-Allow-Origin: *");
-
 
     if( Sentinel::check() ){
 
-
         return 'Залогинен';
-    }else{
 
-//        return response()->json(['resp'=>'Не залогинен']);
+    }else{
 
         return 'Не залогинен';
     }
-
 });
 
