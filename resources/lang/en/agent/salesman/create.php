323e1d100de724522671379b2a1a562733596fce
konst.cos <konst.cos@gmail.com>
2016-10-26 13:33:31 +0300

    add lang in agent create salesman page

---
+++ resources/lang/en/agent/salesman/create.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    // вкладки
+    'general' => 'General',
+
+    // поля формы
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+    'username' => 'Name',
+    'email' => 'E-mail',
+    'password' => 'Password',
+    'password_confirmation' => 'Password confirmation',
+
+    // кнопки
+    'cancel' => 'Cancel',
+    'reset' => 'Reset',
+    'edit' => 'Edit',
+    'create' => 'Create',
+
+
+
+];
