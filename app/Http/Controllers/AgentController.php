bf485a481b0703f73925d7b548cbf09e7677befd
konst.cos <konst.cos@gmail.com>
2016-10-27 13:18:37 +0300

    rearrangement AgentController

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -17,36 +17,76 @@ class AgentController extends BaseController
 //    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     use DispatchesJobs, ValidatesRequests;
 
+
+    /** основные данные */
+
+    // переменная с объектом пользователя
+    public $user;
+
+    // класс пользователя (agent или salesman)
+    public $userClass;
+
+    // кошелек пользователя
+    public $wallet;
+
+
+
+
+
+    // конструктор класса
     public function __construct()
     {
+        // получение id пользователя
         $this->uid = Sentinel::getUser()->id;
-        $this->user = NULL;
-        if(Sentinel::inRole('agent')) {
-            $agent = Agent::findOrFail($this->uid);
-            $this->user = $agent;
-            $this->userClass = 'Agent';
-            $wallet=$agent->wallet()->first();
 
-            $sphere = $agent->sphere();
-            $sphere_id=$sphere->id;
+
+        /** Проверка роли пользователя (salesman или agent) */
+
+        if(Sentinel::inRole('agent')) {
+            // если агент
+
+            // получаем объект агента
+            $this->user = Agent::findOrFail($this->uid);
+            // устанавливаем класс пользователя
+            $this->userClass = 'Agent';
 
         } elseif(Sentinel::inRole('salesman')) {
-            $salesman = Salesman::findOrFail($this->uid);
-            $this->user = $salesman;
+            // если продавец (salesman)
+
+            // получаем объект продавца
+            $this->user  = Salesman::findOrFail($this->uid);
+            // устанавливаем класс продавца
             $this->userClass = 'Salesman';
-            $wallet=$salesman->wallet()->first();
-            $sphere_id=$salesman->sphere()->id;
+
         } else {
+            // если ни одна роль не подходит
+
+            // переходим на страницу логина
             return redirect()->route('login');
         }
 
+        // получение данных по кошельку
+        $wallet = $this->user->wallet()->first();
+        // получение данных по сфере
+        $sphere = $this->user->sphere();
+        // id сферы
+        $sphere_id = $sphere->id;
+
+        // получение строки маски агента
         $this->mask = new AgentBitmask($sphere_id,$this->uid);
         $price = $this->mask->getStatus()->first();
 
+
+        /**
+         * Данные агента по средствам
+         *
+         * количество лидов, которое может купить агент
+         *
+         */
         $balance = ( $price && $price->lead_price && $wallet )?floor($wallet->balance/$price->lead_price):0;
 
-
-
         view()->share('balance', [0, $balance]);
+
+        return true;
     }
 }
