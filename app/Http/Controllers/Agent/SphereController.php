01cac65488331fa15a56b004f9bac1e062afb5e3
konst.cos <konst.cos@gmail.com>
2016-10-31 17:17:12 +0200

    make salesman_balance in the obtain page

--- app/Http/Controllers/Agent/SphereController.php
+++ app/Http/Controllers/Agent/SphereController.php
@@ -103,7 +103,7 @@ class SphereController extends AgentController {
             $balanceJSON = json_encode($balance);
 
             // добавляем на страницу куки с данными по балансу
-            Cookie::queue('balance', $balanceJSON, null, null, null, false, false);
+            Cookie::queue('salesman_balance', $balanceJSON, null, null, null, false, false);
 
         } else {
 
