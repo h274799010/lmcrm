a3581e99e38e2517475b01e3c1ff84d62381e76e
konst.cos <konst.cos@gmail.com>
2016-11-03 23:20:38 +0200

    fixed: method SentinelLeadbayerUser

--- app/Http/Middleware/SentinelLeadbayerUser.php
+++ app/Http/Middleware/SentinelLeadbayerUser.php
@@ -16,12 +16,30 @@ class SentinelLeadbayerUser
      */
     public function handle($request, Closure $next)
     {
+        // текущий агент
         $user = Sentinel::getUser();
-        $admin = Sentinel::findRoleBySlug('leadbayer');
+        // роль, которой агент должен соответствовать
+        $role = Sentinel::findRoleBySlug('leadbayer');
 
-        if (!$user->inRole($admin)) {
+        // если такой роли несуществует
+        if( !$role ){
+            // возвращаемся на главную страницу
             return redirect()->route('home');
         }
+
+        // фильтруем все роли агента, находим соответствие заданной роли
+        $filteredRoles = $user->roles->filter(function( $userRole ) use ($role){
+            // возвращаем роль если id роли агента соответствует id заданной роли
+            return $userRole->id == $role->id;
+        });
+
+        // если заданная роль не соответствует ни одной роли агента
+        if ( $filteredRoles->count() == 0 ) {
+            // возвращаемся на главную страницу
+            return redirect()->route('home');
+        }
+
+        // продолжаем реквест
         return $next($request);
     }
 }
