0ae5b935854007aa14df198bc2bcef058d06cd49
konst.cos <konst.cos@gmail.com>
2016-11-25 16:52:01 +0200

    Partials head: was added outline of future actions

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -14,9 +14,11 @@
     @show
     <!-- Material Design fonts -->
     <link rel="stylesheet" href="{{ asset('components/google-fonts/google-fonts.css')}}">
+    {{-- todo сделать <link rel="stylesheet" href="{{ asset('web/fonts/google-roboto-fonts.css')}}">--}}
+
     {{-- todo <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">--}}
     <link rel="stylesheet" href="{{ asset('components/google-fonts/google-icons.css')}}">
-    {{-- todo <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">--}}
+    {{-- todo  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">--}}
     <link rel="stylesheet" href="{{ asset('components/bootstrap-awesome/font-awesome.min.css')}}">
     {{-- todo <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">--}}
     <!-- Bootstrap -->
@@ -42,8 +44,6 @@
     <script src="{{ asset('components/IE8_support/html5shiv.min.js')}}"></script>
     <script src="{{ asset('components/IE8_support/respond.min.js')}}"></script>
     <![endif]-->
-    {{-- todo <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>--}}
-    {{-- todo <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>--}}
 
 
     <!-- jQuery -->
