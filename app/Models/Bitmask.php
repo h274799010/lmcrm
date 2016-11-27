03e3726b3312db8b34e71436d85dd0c9d71f7ea6
konst.cos <konst.cos@gmail.com>
2016-11-27 21:31:27 +0200

    Bitmask: add method setFbByFields

--- app/Models/Bitmask.php
+++ app/Models/Bitmask.php
@@ -713,6 +713,30 @@ class Bitmask extends Model
 
 
     /**
+     * Установка значений опций полей "ad_" через массив поле=>значение
+     *
+     *
+     * @param array $fieldsData
+     * @param integer|NULL $lead_id
+     *
+     * @return Bitmask
+     */
+    public function setFbByFields( $fieldsData, $lead_id=NULL ){
+
+        // id лида
+        $lead_id = ($lead_id) ? $lead_id : $this->userID;
+
+        // если id лида нет - останавливаем метод
+        if(!$lead_id){ return false; }
+
+        // сохранение значения в БД
+        $this->where('user_id','=',$lead_id)->update( $fieldsData );
+
+        return $this->where('user_id','=',$lead_id)->first();
+    }
+
+
+    /**
      * Установка значения опция атрибута по id маски
      * аналог setAttr() только в отличии от него не делает
      * запрос к бд, а работает с уже готовыми данными
