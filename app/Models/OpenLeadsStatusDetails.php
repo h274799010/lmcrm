5f9e970a8c0c2c6aa10fa91f823a4899598fb96c
konst.cos <konst.cos@gmail.com>
2017-01-23 06:54:47 +0200

    Fixed: logging from 0 status

--- app/Models/OpenLeadsStatusDetails.php
+++ app/Models/OpenLeadsStatusDetails.php
@@ -53,7 +53,6 @@ class OpenLeadsStatusDetails extends Model
         $currentStatus = SphereStatuses::find($newStatus);
         $previous_status = [];
 
-//        dd($currentStatus);
 
 
         // если статус bad
@@ -84,15 +83,46 @@ class OpenLeadsStatusDetails extends Model
             $item->each(function($status) use (&$statusesByType, &$previous_status, &$previous_status_id){
                 // сохраняем данные в массивы
 
-                // данные предыдущего статуса
-                if( $status->id == $previous_status_id){
-                    $previous_status = $status;
+                // действия в зависимости от предыдущего статуса
+                if($previous_status_id == 0){
+                    // если статус нулевой
+
+                    // выбираем статус из рабочих с позицией 1
+                    if( $status->type == 1 && $status->position == 1){
+                        $previous_status = $status;
+                    }
+
+                }else{
+                    // если статус не нулевой
+
+                    // выбираем данные статусо относительно его id
+                    if( $status->id == $previous_status_id){
+                        $previous_status = $status;
+                    }
                 }
+
 
                 $statusesByType[$status->type][$status->position] = $status->toArray();
             });
         });
 
+
+
+
+        // todo если предыдущий статус 0
+        if( $previous_status_id == 0 ){
+            // делаем запись в БД от 0 статуса к первому (статус который имеет позицию один в процессных,
+            // дальше по сценарию)
+
+            $status = new OpenLeadsStatusDetails();
+
+            $status->open_lead_id = $open_lead_id;
+            $status->user_id = $user_id;
+            $status->previous_status_id = 0;
+            $status->status_id = $previous_status['id'];
+
+            $status->save();
+        }
 
 
         // если статус Process
@@ -107,7 +137,7 @@ class OpenLeadsStatusDetails extends Model
 
                 $status->open_lead_id = $open_lead_id;
                 $status->user_id = $user_id;
-                $status->previous_status_id = $previous_status_id;
+                $status->previous_status_id = $previous_status['id'];
                 $status->status_id = $newStatus;
 
                 $status->save();
@@ -151,7 +181,7 @@ class OpenLeadsStatusDetails extends Model
 
                     $status->open_lead_id = $open_lead_id;
                     $status->user_id = $user_id;
-                    $status->previous_status_id = $previous_status_id;
+                    $status->previous_status_id = $previous_status['id'];
                     $status->status_id = $newStatus;
 
                     $status->save();
@@ -246,7 +276,7 @@ class OpenLeadsStatusDetails extends Model
 
                     $status->open_lead_id = $open_lead_id;
                     $status->user_id = $user_id;
-                    $status->previous_status_id = $previous_status_id;
+                    $status->previous_status_id = $previous_status['id'];
                     $status->status_id = $newStatus;
 
                     $status->save();
